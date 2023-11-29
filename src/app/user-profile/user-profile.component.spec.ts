7bf44e1bbacd673f2110144bb063e483ccacd273
Johnnie432 <johnnie432@icloud.com>
2023-11-28 20:05:25 -0500

    fixed broken test

--- src/app/user-profile/user-profile.component.spec.ts
+++ src/app/user-profile/user-profile.component.spec.ts
@@ -4,7 +4,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
 
-fdescribe('UserProfileComponent', () => {
+describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
   let fixture: ComponentFixture<UserProfileComponent>;
   let router: jasmine.SpyObj<Router>;
@@ -13,19 +13,27 @@ fdescribe('UserProfileComponent', () => {
     TestBed.configureTestingModule({
       declarations: [UserProfileComponent],
       imports: [HttpClientTestingModule, RouterTestingModule],
-      providers: []
+      providers: [{ provide: Router, useClass: mockRouter}]
     });
     fixture = TestBed.createComponent(UserProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  beforeEach(()=>{
-    router = TestBed.get(Router);
-    spyOn(router, 'getCurrentNavigation').and.returnValue({ extras: { state: { newUser: 'false'} } } as any);
-  })
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
+class mockRouter{
+  getCurrentNavigation(){
+    return {
+      extras: {
+        state: {
+          newUser: false
+        }
+      }
+    }
+  }
+}
